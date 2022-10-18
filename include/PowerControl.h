2af0dd16106f04a0eae99b0d74f161ae0546f270
Maksym Lunin <maksym.lunin@gmail.com>
2022-10-18 03:16:34 +0300

    Animated charging

--- include/PowerControl.h
+++ include/PowerControl.h
@@ -2,26 +2,22 @@
 #define __POWER_CONTROL_H__
 
 #include <cstdint>
+#include <M5Stack.h>
 
-class M5Display;
-class TFT_eSprite;
 
 class PowerControl {
+struct State;
 private:
-    TFT_eSprite *sprite;
+    TFT_eSprite sprite;
 public:
     PowerControl(M5Display *);
     ~PowerControl();
+    uint16_t color = TFT_WHITE;
+    uint16_t background = TFT_BLACK;
+    int16_t width();
+    int16_t height();
 
-    uint16_t color;
-    uint16_t background;
-
-    void setColor(uint16_t);
-    void setBackground(uint16_t);
-    uint16_t width() const;
-    uint16_t height() const;
-
-    void drawAt(uint16_t x, uint16_t y);
+    void drawAt(int16_t x, int16_t y);
 };
 
 #endif //__POWER_CONTROL_H__
--- src/PowerControl.cpp
+++ src/PowerControl.cpp
@@ -1,61 +1,53 @@
 #include "PowerControl.h"
 
-#include <M5Stack.h>
-#include <Arduino.h>
-
-PowerControl::PowerControl(M5Display *display) {
-    sprite = new TFT_eSprite(display);
-
-    sprite->setColorDepth(8);
-    sprite->createSprite(16, 10);
-    // sprite->fillSprite(TFT_TRANSPARENT);
-    color = WHITE;
-    background = BLACK;
+PowerControl::PowerControl(M5Display *display)
+: sprite(TFT_eSprite(display))
+ {
+    sprite.setColorDepth(8);
+    sprite.createSprite(16, 10);
 }
 
 PowerControl::~PowerControl() {
-    delete sprite;
 }
 
-uint16_t PowerControl::width() const {
-    return sprite->width();
+int16_t PowerControl::width() {
+    return sprite.width();
 }
 
-uint16_t PowerControl::height() const {
-    return sprite->height();
+int16_t PowerControl::height() {
+    return sprite.height();
 }
 
-void PowerControl::setColor(uint16_t color) {
-    this->color = color;
-}
+void PowerControl::drawAt(int16_t x, int16_t y) {
+    auto time = millis() % 1000;
+    bool isOn = time > 500;
 
-void PowerControl::setBackground(uint16_t color) {
-    this->background = color;
-}
+    bool isChargeFull = M5.Power.isChargeFull();
+    bool isCharging = M5.Power.isCharging();
 
-void PowerControl::drawAt(uint16_t x, uint16_t y) {
-    sprite->fillSprite(background);
-    // Draw frame
-
+    sprite.fillSprite(background);
     
-    bool drawFrame = true;
-
-    const auto line_width = 3;
-
-    if (drawFrame) {
-        sprite->drawLine(0, 1, 0, 7, color);
-        sprite->drawLine(1, 0, 13, 0, color);
-        sprite->drawLine(1, 9, 13, 9, color);
-        sprite->drawLine(14, 1, 14, 8, color);
-        sprite->drawLine(15, 3, 15, 6, color);
-    };
-
-    auto lines = (M5.Power.getBatteryLevel() / 25) * line_width;
-    for (int8_t i = 0; i < lines; i+=line_width) {
-        int n = i + 2;
-        sprite->drawLine(n,   2, n,   7, color);
-        sprite->drawLine(n+1, 2, n+1, 7, color);
+    // Draw frame
+    sprite.drawLine(0, 1, 0, 8, color);
+    sprite.drawLine(1, 0, 13, 0, color);
+    sprite.drawLine(1, 9, 13, 9, color);
+    sprite.drawLine(14, 1, 14, 8, color);
+    if (isCharging) {
+        if (isOn) {
+            sprite.drawLine(15, 3, 15, 6, color);
+        }
+    } else {
+        sprite.drawLine(15, 3, 15, 6, color);
+    } 
+    
+    const int level_bars = M5.Power.getBatteryLevel() / 25;
+    const int display_bars = min(4, level_bars + ((isCharging && isOn) ? 1:0));
+    
+    for (int i = 0; i < display_bars; ++i) {
+        int n = i*3 + 2;
+        sprite.drawLine(n,   2, n,   7, color);
+        sprite.drawLine(n+1, 2, n+1, 7, color);
     }
 
-    sprite->pushSprite(x, y);
+    sprite.pushSprite(x, y);
 }
