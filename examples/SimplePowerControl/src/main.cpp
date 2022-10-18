eeac48535cacb7f95d24030c628443b3d02170db
Maksym Lunin <maksym.lunin@gmail.com>
2022-10-18 12:29:19 +0300

    Example update

--- examples/SimplePowerControl/src/main.cpp
+++ examples/SimplePowerControl/src/main.cpp
@@ -4,15 +4,19 @@
 PowerControl power = PowerControl(&M5.Lcd);
 
 void setup() {
-    M5.begin();               // Initialise the display
+    // Initialise m5stack:
+    M5.begin();   
     M5.Power.begin();
+
+    // Optional: set colors:
+    power.color = TFT_WHITE;
+    power.charging = TFT_GREEN;
+    power.background = TFT_TRANSPARENT;
 }
 
 void loop() {
-
     // Draw battery on upper left corner
     power.drawAt(0, 0); 
-
 
     M5.update(); 
     if (M5.BtnA.pressedFor(3000)) {
